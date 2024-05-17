4c16e61bf50ddae55eaab7a5c490da34350c9208
Lisandro Corbalan <lmcorbalan@gmail.com>
2024-05-17 11:25:53 -0300

    MIT license

--- src/L1XERC20Gateway.sol
+++ src/L1XERC20Gateway.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.25;
 
 import { L1CustomGateway } from "@arbitrum/tokenbridge/ethereum/gateway/L1CustomGateway.sol";
--- src/libraries/L1ArbitrumEnabled.sol
+++ src/libraries/L1ArbitrumEnabled.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.25;
 
 import { IL1CustomGateway } from "src/interfaces/IL1CustomGateway.sol";
