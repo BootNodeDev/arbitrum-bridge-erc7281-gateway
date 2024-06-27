ee7251f01cbab687a56820da1561737d8f0b55f1
Lisandro Corbalan <lmcorbalan@gmail.com>
2024-06-27 10:25:19 -0300

    Move Ownable from XERC20BaseAdapter to L1EXERC20Adapter

--- test/L1XERC20AdapterTest.t.sol
+++ test/L1XERC20AdapterTest.t.sol
@@ -13,6 +13,17 @@ contract L1XERC20AdapterTest is XERC20BaseAdapterTest {
         assertEq(L1XERC20Adapter(_adapter).isArbitrumEnabled(), uint8(0xb1));
     }
 
+    function test_registerTokenOnL2_OnlyOwner() public {
+        address caller = makeAddr("someOther");
+        deal(caller, 2);
+
+        vm.prank(caller);
+        vm.expectRevert("Ownable: caller is not the owner");
+        L1XERC20Adapter(_adapter).registerTokenOnL2{ value: 2}(
+            makeAddr("l2Token"), 0, 0, 0, 0, 0, 1, 1, makeAddr("creditBack")
+        );
+    }
+
     function _createAdapter() internal override {
         _adapter = address(new L1XERC20Adapter(address(xerc20), makeAddr("gateway"), _owner));
     }
